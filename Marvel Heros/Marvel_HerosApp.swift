c2b3db8e24ce416405f15dd498639952056fee8e
Ahmed Naser <ahmed.nasser2310@gmail.com>
2022-01-07 19:11:39 +0200

    Initial Commit

---
+++ Marvel Heros/Marvel_HerosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Marvel_HerosApp.swift
+//  Marvel Heros
+//
+//  Created by Ahmed Naser on 07/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Marvel_HerosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
