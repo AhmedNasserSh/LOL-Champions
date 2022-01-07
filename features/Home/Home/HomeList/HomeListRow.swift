9710dffd927fc2eaddf48179875249d5c0432ecb
Ahmed Naser <ahmed.nasser2310@gmail.com>
2022-01-07 21:09:25 +0200

    add workspace

---
+++ features/Home/Home/HomeList/HomeListRow.swift
@@ -0,0 +1,20 @@
+//
+//  HomeListRow.swift
+//  Home
+//
+//  Created by Ahmed Naser on 07/01/2022.
+//
+
+import SwiftUI
+
+struct HomeListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeListRow_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeListRow()
+    }
+}
