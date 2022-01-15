bdbf80ed8d5a802eadaa40fed661eaff5bc9e1de
Ahmed Naser <ahmed.nasser2310@gmail.com>
2022-01-15 19:36:37 +0200

    add screenshots

---
+++ Pods/Target Support Files/Pods-Common-CommonTests/Pods-Common-CommonTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Common_CommonTests : NSObject
+@end
+@implementation PodsDummy_Pods_Common_CommonTests
+@end
---
+++ Pods/Target Support Files/Pods-NetworkManger-NetworkMangerTests/Pods-NetworkManger-NetworkMangerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NetworkManger_NetworkMangerTests : NSObject
+@end
+@implementation PodsDummy_Pods_NetworkManger_NetworkMangerTests
+@end
