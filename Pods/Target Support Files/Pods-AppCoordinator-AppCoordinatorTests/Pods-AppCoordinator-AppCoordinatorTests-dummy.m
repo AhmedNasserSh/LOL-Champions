0d8761e2bae9052aa278356fb52ffa349201145f
Ahmed Naser <ahmed.nasser2310@gmail.com>
2022-01-16 10:24:03 +0200

    change module screenshot

---
+++ Pods/Target Support Files/Pods-AppCoordinator-AppCoordinatorTests/Pods-AppCoordinator-AppCoordinatorTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppCoordinator_AppCoordinatorTests : NSObject
+@end
+@implementation PodsDummy_Pods_AppCoordinator_AppCoordinatorTests
+@end
---
+++ Pods/Target Support Files/Pods-LOLChampions-LOLChampionsTests/Pods-LOLChampions-LOLChampionsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LOLChampions_LOLChampionsTests : NSObject
+@end
+@implementation PodsDummy_Pods_LOLChampions_LOLChampionsTests
+@end
